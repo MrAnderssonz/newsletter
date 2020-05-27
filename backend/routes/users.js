1299f5e9818dd08e41f6c665392c6528a184ea09
Johan Andersson <ewan.andersson@gmail.com>
2020-05-27 15:46:50 +0200

    user file and post added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
+var fs = require('fs');
+
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  fs.readFile('users.json', (err, data) => 
+  {
+    if (err)
+    {
+      throw err;
+    }
+    var users = JSON.parse(data);
+    res.send(users);
+  })
+
 });
 
+router.post('/', function(req, res, next) {
+  fs.readFile('users.json', (err, data) => 
+  {
+    if (err)
+    {
+      throw err;
+    }
+    var users = JSON.parse(data);
+
+    newUser = 
+    {
+      "id": req.body.id,
+      "userName": req.body.userName,
+      "mail": req.body.mail
+    }
+
+    users.push(newUser);
+    var saveUsers = JSON.stringify(users, null, 2);
+
+    fs.writeFile('users.json', saveUsers, (err, data) =>
+    {
+      if (err)
+      {
+        throw err;
+      }
+    });
+
+    res.send("Ny använade sparad");
+  })
+
+});
 module.exports = router;
